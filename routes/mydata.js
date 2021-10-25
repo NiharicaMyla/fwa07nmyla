6c70a86d4fb5570a1f8ab2a6ee0dd69568d92320
Niharica Myla <89415756+NiharicaMyla@users.noreply.github.com>
2021-10-25 00:21:33 -0500

    changes

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("mydata", { title: "Niharica Myla", fact: "travelling" });
+  res.render("mydata", { title: "Niharica Myla", fact: "fact" });
 });
 
 module.exports = router;
